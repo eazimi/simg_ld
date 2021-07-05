d42e1e3a84d6f0e9f87e3198cea6a3c2cbbb2cd8
Ehsan Azimi <eazimi@ehsan.irisa.fr>
2021-07-05 12:39:42 +0200

    cleanup

--- app_loader.cpp
+++ app_loader.cpp
@@ -51,12 +51,10 @@ void AppLoader::memUnmapRanges()
         auto start_str = addr_range_str.substr(0, dash_index);
         auto end_str = addr_range_str.substr(dash_index+1);
         auto dash_index_last = line.find_last_of(" ");        
-        // cout << "found: " << start_str << ", " << end_str << "    " << line.substr(dash_index_last+1) << endl;
         start_str += "0x";
         end_str += "0x";
         auto start_int = strtol(start_str.c_str(), nullptr , 0);
         auto end_int = strtol(end_str.c_str(), nullptr, 0);
-        // cout << "address: " << start_int << ", " << end_int << endl;
         auto ret = munmap((void *)((unsigned long)start_int), (unsigned long)(end_int-start_int));
         if(ret != 0)
           cout << "munmap was not successful: " << strerror(errno) << endl;
--- simg_ld.cpp
+++ simg_ld.cpp
@@ -7,6 +7,5 @@ int main(int argc, char **argv, char** env)
     std::unique_ptr<Loader> loader = std::make_unique<Loader>();
     loader->init(argc);
     loader->run(argv);
-    std::cout << "hello world!" << std::endl;
     return 0;
 }
