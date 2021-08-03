6b683cbe04f9e8b8ef492ae19aaced782d9ee36f
Ehsan Azimi <eazimi@ehsan.irisa.fr>
2021-08-03 11:26:20 +0200

    send() and receive() overloaded

--- channel.hpp
+++ channel.hpp
@@ -9,6 +9,10 @@ class Channel
 {
 private:
     int socket_{-1};
+    template<class M> static constexpr bool messageType()
+    {
+        return std::is_trivial<M>::value && std::is_class<M>::value;
+    }
 
 public:
     explicit Channel(int socket) : socket_(socket) {}
@@ -18,8 +22,21 @@ public:
     Channel(const Channel &) = delete;
     Channel& operator=(const Channel &) = delete;
 
+    // send
     int send(const void *message, size_t size) const;
+    template <class M>
+    inline typename std::enable_if_t<messageType<M>(), int> send(M const &m) const
+    {
+        return this->send(&m, sizeof(M));
+    }
+
+    // receive
     size_t receive(void *message, size_t size, bool block = true) const;
+    template <class M>
+    inline typename std::enable_if_t<messageType<M>(), ssize_t> receive(M &m) const
+    {
+        return this->receive(&m, sizeof(M));
+    }
 
     inline int get_socket() const { return socket_; }
 };
