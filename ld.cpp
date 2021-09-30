e302cad3c89999db5b9b7d5de325bbbfc26d4a2e
Ehsan Azimi <eazimi.ac@gmail.com>
2021-09-30 15:44:06 +0200

    get_interpreter_entry()

--- ld.cpp
+++ ld.cpp
@@ -1,1 +1,26 @@
 #include "ld.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+#include <cstring>
+
+Elf64_Addr LD::get_interpreter_entry(const char* ld_name)
+{
+  int rc;
+  char e_ident[EI_NIDENT];
+
+  int fd = open(ld_name, O_RDONLY);
+  assert(fd != -1);
+
+  rc = read(fd, e_ident, sizeof(e_ident));
+  assert(rc == sizeof(e_ident));
+  assert(strncmp(e_ident, ELFMAG, strlen(ELFMAG)) == 0);
+  assert(e_ident[EI_CLASS] == ELFCLASS64); // FIXME:  Add support for 32-bit ELF
+
+  // Reset fd to beginning and parse file header
+  lseek(fd, 0, SEEK_SET);
+  Elf64_Ehdr elf_hdr;
+  rc = read(fd, &elf_hdr, sizeof(elf_hdr));
+  assert(rc == sizeof(elf_hdr));
+  return elf_hdr.e_entry;
+}
--- ld.h
+++ ld.h
@@ -1,8 +1,12 @@
 #ifndef LD_H
 #define LD_H
 
+#include "elf.h"
+
 class LD {
 private:
+  Elf64_Addr get_interpreter_entry(const char* ld_name);
+
 public:
   explicit LD() = default;
 };
