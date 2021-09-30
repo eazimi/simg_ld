70149e5bdb69b8ea69e6cf456f50252596020610
Ehsan Azimi <eazimi.ac@gmail.com>
2021-09-30 18:00:48 +0200

    more functions

--- rtld.cpp
+++ rtld.cpp
@@ -1,1 +1,9 @@
 #include "rtld.h"
+
+RTLD::RTLD()
+{
+  vm_ = make_unique<user_space>();
+  ld_ = make_unique<LD>();
+}
+
+void RTLD::run() {}
--- rtld.h
+++ rtld.h
@@ -1,10 +1,19 @@
 #ifndef RTLD_H
 #define RTLD_H
 
+#include "ld.h"
+#include "user_space.h"
+
+using namespace std;
+
 class RTLD {
-    private:
-    public:
-        explicit RTLD() = default;
+private:
+  unique_ptr<LD> ld_;
+  unique_ptr<user_space> vm_;
+
+public:
+  explicit RTLD();
+  void run();
 };
 
 #endif
