011f0aabb06430d6c46eabfd21f6cb5b0c3bc05b
Ehsan Azimi <eazimi.ac@gmail.com>
2021-09-30 16:28:16 +0200

    map_elf_interpreter_load_segment()

--- ld.cpp
+++ ld.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <assert.h>
 #include <cstring>
+#include "global.hpp"
 
 Elf64_Addr LD::get_interpreter_entry(const char* ld_name)
 {
@@ -23,4 +24,73 @@ Elf64_Addr LD::get_interpreter_entry(const char* ld_name)
   rc = read(fd, &elf_hdr, sizeof(elf_hdr));
   assert(rc == sizeof(elf_hdr));
   return elf_hdr.e_entry;
+}
+
+unsigned long LD::map_elf_interpreter_load_segment(void* startAddr, int fd, Elf64_Ehdr* ehdr, Elf64_Phdr* phdr)
+{
+  unsigned long minva, maxva;
+  Elf64_Phdr* iter;
+  ssize_t sz;
+  int flags, dyn = ehdr->e_type == ET_DYN;
+  unsigned char *p, *base;
+
+  minva = (unsigned long)-1;
+  maxva = 0;
+
+  for (iter = phdr; iter < &phdr[ehdr->e_phnum]; iter++) {
+    if (iter->p_type != PT_LOAD)
+      continue;
+    if (iter->p_vaddr < minva)
+      minva = iter->p_vaddr;
+    if (iter->p_vaddr + iter->p_memsz > maxva)
+      maxva = iter->p_vaddr + iter->p_memsz;
+  }
+
+  minva = TRUNC_PG(minva);
+  maxva = ROUND_PG(maxva);
+
+  /* For dynamic ELF let the kernel chose the address. */
+  // hint = dyn ? NULL : (unsigned char *)minva;
+  flags = dyn ? 0 : MAP_FIXED;
+  flags |= (MAP_PRIVATE | MAP_ANONYMOUS);
+
+  /* Check that we can hold the whole image. */
+  // base = (unsigned char*) mmap(hint, maxva - minva, PROT_NONE, flags, -1, 0);
+  base = (unsigned char*)mmap(startAddr, maxva - minva, PROT_NONE, flags, -1, 0);
+  if (base == (void*)-1)
+    return -1;
+  munmap(base, maxva - minva);
+
+  // base = (unsigned char*) g_range->start;
+
+  flags = MAP_FIXED | MAP_ANONYMOUS | MAP_PRIVATE;
+  // flags = MAP_FIXED | MAP_PRIVATE;
+  /* Now map each segment separately in precalculated address. */
+  for (iter = phdr; iter < &phdr[ehdr->e_phnum]; iter++) {
+    unsigned long off, start;
+    if (iter->p_type != PT_LOAD)
+      continue;
+    off   = iter->p_vaddr & ALIGN;
+    start = dyn ? (unsigned long)base : 0;
+    // start = (unsigned long)g_range->start;
+    start += TRUNC_PG(iter->p_vaddr);
+    sz = ROUND_PG(iter->p_memsz + off);
+
+    p = (unsigned char*)mmap((void*)start, sz, PROT_WRITE, flags, -1, 0);
+    if (p == (void*)-1) {
+      munmap(base, maxva - minva);
+      return LOAD_ERR;
+    }
+    if (lseek(fd, iter->p_offset, SEEK_SET) < 0) {
+      munmap(base, maxva - minva);
+      return LOAD_ERR;
+    }
+    if (read(fd, p + off, iter->p_filesz) != (ssize_t)iter->p_filesz) {
+      munmap(base, maxva - minva);
+      return LOAD_ERR;
+    }
+    mprotect(p, sz, PFLAGS(iter->p_flags));
+  }
+
+  return (unsigned long)base;
 }
--- ld.h
+++ ld.h
@@ -6,6 +6,7 @@
 class LD {
 private:
   Elf64_Addr get_interpreter_entry(const char* ld_name);
+  unsigned long map_elf_interpreter_load_segment(void* startAddr, int fd, Elf64_Ehdr* ehdr, Elf64_Phdr* phdr);
 
 public:
   explicit LD() = default;
