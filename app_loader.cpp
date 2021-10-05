3d1cf8bd0839474aeb24d6cdb2643ef7f74fd72f
Ehsan Azimi <eazimi.ac@gmail.com>
2021-10-05 10:58:09 +0200

    runRtld()

--- app_loader.cpp
+++ app_loader.cpp
@@ -150,3 +150,53 @@ DynObjInfo AppLoader::load_lsdo(void* startAddr, const char* ld_name)
   info.set_entry_point(entryPoint);
   return info;
 }
+
+// This function loads in ld.so, sets up a separate stack for it, and jumps
+// to the entry point of ld.so
+void AppLoader::runRtld(int param_index, int param_count, int socket_id)
+{
+  int rc = -1;
+  auto startAddr = user_space_->getStartAddr();
+  // Load RTLD (ld.so)
+  DynObjInfo ldso = load_lsdo(startAddr, (char*)LD_NAME);
+
+  if (ldso.get_base_addr() == NULL || ldso.get_entry_point() == NULL) {
+    DLOG(ERROR, "Error loading the runtime loader (%s). Exiting...\n", (char*)LD_NAME);
+    return;
+  }
+  cout << "ldso addr: " << std::hex << startAddr << endl;
+
+  // Create new stack region to be used by RTLD
+  auto stackStartAddr = (void*)((unsigned long)startAddr + GB1);
+  void* newStack = user_space_->createNewStack(ldso, stackStartAddr, param_index, param_count, socket_id);
+  cout << "stack addr: " << std::hex << stackStartAddr << endl;
+  if (!newStack) {
+    DLOG(ERROR, "Error creating new stack for RTLD. Exiting...\n");
+    exit(-1);
+  }
+
+  // Create new heap region to be used by RTLD
+  void* heapStartAddr = (void*)((unsigned long)startAddr + GB2);
+  cout << "heap addr: " << std::hex << heapStartAddr << endl;
+  void* newHeap = user_space_->createNewHeap(heapStartAddr);
+  if (!newHeap) {
+    DLOG(ERROR, "Error creating new heap for RTLD. Exiting...\n");
+    exit(-1);
+  }
+
+  std::stringstream ss;
+  ss << ((getpid() == _parent_pid) ? "[PARENT], " : "[CHILD], ") << "before jumping to sp: " << std::dec << getpid();
+  pause_run(ss.str());
+  // print_mmapped_ranges(getpid());
+  write_mmapped_ranges("before_jump_run_rtld", getpid());
+
+  // Pointer to the ld.so entry point
+  void* ldso_entrypoint = ldso.get_entry_point();
+
+  // Change the stack pointer to point to the new stack and jump into ld.so
+  asm volatile(CLEAN_FOR_64_BIT(mov %0, %%esp;) : : "g"(newStack) : "memory");
+  asm volatile("jmp *%0" : : "g"(ldso_entrypoint) : "memory");
+
+  DLOG(ERROR, "Error: RTLD returned instead of passing the control to the created stack. Panic...\n");
+  exit(-1);
+}
--- app_loader.h
+++ app_loader.h
@@ -11,14 +11,14 @@ using namespace std;
 class AppLoader {
 private:
   unique_ptr<user_space> user_space_;
-  Elf64_Addr get_interpreter_entry(const char* ld_name);  
+  Elf64_Addr get_interpreter_entry(const char* ld_name);
   unsigned long loadSegment(void* startAddr, int fd, Elf64_Ehdr* ehdr, Elf64_Phdr* phdr);
   void* loadInterpreter(void* startAddr, const char* elf_interpreter, DynObjInfo& info);
+  DynObjInfo load_lsdo(void* startAddr, const char* ld_name);
 
 public:
   explicit AppLoader();
-  explicit AppLoader() = default;
-  DynObjInfo load_lsdo(void* startAddr, const char* ld_name);
+  void runRtld(int param_index, int param_count, int socket_id);
   inline void ReserveMemSpace(unsigned long size) const { user_space_->reserve_mem_space(size); }
   inline void* getReservedSpaceStartAddr() const { return user_space_->getStartAddr(); }
   inline unsigned long getReservedSpaceSize() const { return user_space_->getSize(); }
