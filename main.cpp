dc05ebaa3af1921973fe3bc94f5c56fe68fa1951
Ehsan Azimi <eazimi.ac@gmail.com>
2021-10-12 17:49:35 +0200

    static link for simgld, dynamic link for libevent

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv, char** env)
+{
+  cout << "Three Executable, static linking for simgld, dynamic linking for libevent" << endl;
+  return 0;
+}
