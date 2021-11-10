7de10465a66db3d426a2fbc05de7fa3e36e2cecd
Ehsan Azimi <eazimi.ac@gmail.com>
2021-11-10 09:30:14 +0100

    for debugging

--- main.cpp
+++ main.cpp
@@ -4,6 +4,37 @@ using namespace std;
 
 int main(int argc, char** argv, char** env)
 {
+  // auto ret_setenv = setenv("LD_PRELOAD", "/home/eazimi/projects/simgld/build/libwrapper.so", 1);
+  // cout << "ret_setend: " << ret_setenv << endl;
+  cout << "LD_PRELOAD=" << getenv("LD_PRELOAD") << endl;
+
+  // filebuf fb;
+  // std::string path = "./main_env.txt";
+  // if (fb.open(path, std::ios_base::out)) {
+  //   std::ostream os(&fb);
+  //   auto p = env;
+  //   while (*p++ != nullptr)
+  //     os << *p << endl;
+  //   fb.close();
+  // }
+  
+  // path = "/etc/ld.so.preload";
+  // cout << "content of " << path << ":" << endl;
+  // if (fb.open(path, std::ios_base::in)) {
+  //   std::istream is(&fb);
+  //   string line;
+  //   while (getline(is, line))
+  //     cout << line;
+  //   fb.close();
+  // } else {
+  //   cout << "error in oppenning " << path << endl;
+  //   cout << strerror(errno) << endl;
+  // }
+
+  // cout << "main()->argv: " << endl;
+  // while(*argv++ != nullptr)
+  //   cout << *argv << endl;
+
   unique_ptr<AppLoader> appLoader = make_unique<AppLoader>();
   auto mcAddr              = appLoader->reserveMemSpace(MB5500, GB2);
   auto appAddr                   = appLoader->reserveMemSpace(GB3, GB2);
--- mc_main.cpp
+++ mc_main.cpp
@@ -2,6 +2,16 @@
 
 int main(int argc, char** argv, char** env)
 {
+  // filebuf fb;
+  // std::string path = "./mc_main_env.txt";
+  // if (fb.open(path, std::ios_base::out)) {
+  //   std::ostream os(&fb);
+  //   auto p = env;
+  //   while (*p++ != nullptr)
+  //     os << *p << endl;
+  //   fb.close();
+  // }
+
   std::unique_ptr<MC> mc = std::make_unique<MC>();
   mc->run(argv);
   return 0;
